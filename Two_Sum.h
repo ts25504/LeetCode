ee9d855962c266e1f7f83fda090b4e8965ebcbad
ts25504 <ts25504@163.com>
2015-02-21 23:14:04 +0800

    Finish Two Sum.

---
+++ Two_Sum.h
@@ -0,0 +1,73 @@
+/*
+ * Given an array of integers, find two numbers such that they add up to a specific target number.
+ *
+ * The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
+ *
+ * You may assume that each input would have exactly one solution.
+ *
+ * Input: numbers={2, 7, 11, 15}, target=9
+ * Output: index1=1, index2=2
+ */
+
+#ifndef Two_Sum_h
+#define Two_Sum_h
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <utility>
+#include <algorithm>
+
+class Solution {
+public:
+    std::vector<int> twoSum(std::vector<int>& numbers, int target)
+    {
+        std::vector<int> ret;
+        std::multimap<int, int> m;
+        for (int i = 0; i < numbers.size(); ++i)
+            m.insert(std::make_pair(numbers[i], i+1));
+        std::sort(numbers.begin(), numbers.end());
+        int left = 0, right = numbers.size() - 1;
+        int sum = 0;
+        while (left < right)
+        {
+            sum = numbers[left] + numbers[right];
+            if (sum == target)
+            {
+                int min_iter1 = numbers.size();
+                int max_iter1 = 0;
+                int min_iter2 = numbers.size();
+                int max_iter2 = 0;
+                for (auto iter = m.lower_bound(numbers[left]);
+                        iter != m.upper_bound(numbers[left]);
+                        ++iter)
+                {
+                    if (iter->second < min_iter1)
+                        min_iter1 = iter->second;
+                    if (iter->second > max_iter1)
+                        max_iter1 = iter->second;
+                }
+                for (auto iter = m.lower_bound(numbers[right]);
+                        iter != m.upper_bound(numbers[right]);
+                        ++iter)
+                {
+                    if (iter->second < min_iter2)
+                        min_iter2 = iter->second;
+                    if (iter->second > max_iter2)
+                        max_iter2 = iter->second;
+                }
+                ret.push_back(min_iter1 < min_iter2 ? min_iter1 : min_iter2);
+                ret.push_back(max_iter1 > max_iter2 ? max_iter1 : max_iter2);
+                break;
+            }
+            else
+            {
+                sum < target ? ++left : --right;
+            }
+        }
+        return ret;
+    }
+};
+
+#endif
+
