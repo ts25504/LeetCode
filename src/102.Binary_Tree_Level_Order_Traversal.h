0fd86aa26d7c1cbe07738f95e2f989452fc1204d
ts25504 <ts25504@163.com>
2015-03-10 00:03:32 +0800

    Finish problem 101, 102, 103, 104.

---
+++ src/102.Binary_Tree_Level_Order_Traversal.h
@@ -0,0 +1,44 @@
+/*
+ * Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+ */
+
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int> > levelOrder(TreeNode* root)
+    {
+        vector<vector<int> > res;
+        if (root == NULL)
+            return res;
+        queue<TreeNode*> node_queue;
+        queue<TreeNode*> root_queue;
+        int level = 0;
+        node_queue.push(root);
+        while (!node_queue.empty())
+        {
+            res.push_back(vector<int>());
+            while (!node_queue.empty())
+            {
+                TreeNode* node = node_queue.front();
+                root_queue.push(node);
+                node_queue.pop();
+            }
+            while (!root_queue.empty())
+            {
+                TreeNode* node = root_queue.front();
+                res[level].push_back(node->val);
+                root_queue.pop();
+                if (node->left)
+                    node_queue.push(node->left);
+                if (node->right)
+                    node_queue.push(node->right);
+            }
+            ++level;
+        }
+        return res;
+    }
+};
